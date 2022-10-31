ad2e33973560b6e785f8c28424d6f3ad10b367bf
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2022-10-31 23:02:32 +0900

    FutureBuilder의 AnimatedSwitcher 위젯을 사용해서 화면 변경 시 애니메이션이 적용되도록 구현 (  AnimatedSwitcher 위젯)

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,34 @@ class MyApp extends StatelessWidget
 {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreen(),
+    return FutureBuilder<Object>(
+      future : Future.delayed(Duration(seconds: 3), ()=>1),
+      builder: (context, snapshot) {
+        return AnimatedSwitcher(duration: Duration(milliseconds: 300) ,child: _splashLoadingWidget(snapshot));
+      }
     );
+  }
+
+  StatelessWidget _splashLoadingWidget(AsyncSnapshot<Object?> snapshot) {
+    if(snapshot.hasError){
+      print('error occur while loading. ');
+      return Text('Error occur');
+    }else if(snapshot.hasData){
+      return HyerimApp();
+    }else{
+      return SplashScreen();
+    }
   }
 }
 
+class HyerimApp extends StatelessWidget {
+  const HyerimApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
 
