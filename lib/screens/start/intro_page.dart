f30f4cefa6606cf89c8efe8867a767b1f375f90c
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2022-11-03 23:49:51 +0900

    intro_page.dart 의 TextButton 에 onButtonClick 메소드 추가

--- lib/screens/start/intro_page.dart
+++ lib/screens/start/intro_page.dart
@@ -1,10 +1,13 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter2022/utils/logger.dart';
 
 class IntroPage extends StatelessWidget {
   const IntroPage({Key? key}) : super(key: key);
 
-  void onButtonClick(){}
+  void onButtonClick(){
+    logger.d('On TextButton Click');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +15,13 @@ class IntroPage extends StatelessWidget {
       children: [
         Text('Hyerim\'s Market'),
         ExtendedImage.asset('assets/images/together.jpg',scale: 4,),
-        Text('패션 킬러 혜림 솨핑몰'),
-        Text('Feel so Good in Hyerim\'s shopping mall'),
-        TextButton(onPressed: onButtonClick, child: Text('내 동네 설정하고 시작하기'))
+        Text('패션 킬러 혜림 쇼핑몰'),
+        Text('혜림 쇼핑몰은 항상 편하고 개성넘칩니다.\n혜림 쇼핑몰과 함께 당신을 패션으로 표현해보세요'),
+        TextButton(onPressed: onButtonClick,
+          child: Text('내 동네 설정하고 시작하기', style: TextStyle(color: Colors.white),),
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.blue,
+          ))
       ],
     );
   }
