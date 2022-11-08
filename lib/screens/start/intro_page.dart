2c54e514e5cc7a1a8469f1be2b549b9f3bd81f19
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2022-11-09 00:05:48 +0900

    intro_page.dart 의 Text, TextButton에 스타일 설정, Column의 mainAxisAlignment를 spaceEvenly로 설정

--- lib/screens/start/intro_page.dart
+++ lib/screens/start/intro_page.dart
@@ -11,18 +11,22 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text('Hyerim\'s Market'),
-        ExtendedImage.asset('assets/images/together.jpg',scale: 4,),
-        Text('패션 킬러 혜림 쇼핑몰'),
-        Text('혜림 쇼핑몰은 항상 편하고 개성넘칩니다.\n혜림 쇼핑몰과 함께 당신을 패션으로 표현해보세요'),
-        TextButton(onPressed: onButtonClick,
-          child: Text('내 동네 설정하고 시작하기', style: TextStyle(color: Colors.white),),
-          style: TextButton.styleFrom(
-            backgroundColor: Colors.blue,
-          ))
-      ],
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text('Hyerim\'s Market', style: TextStyle(color:Colors.blue, fontSize:35, fontWeight: FontWeight.w800 )),
+          ExtendedImage.asset('assets/images/together.jpg',scale: 4,),
+          Text('패션 킬러 혜림 쇼핑몰',style:TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
+          Text('혜림 쇼핑몰은 항상 편하고 개성넘칩니다.\n혜림 쇼핑몰과 함께 당신을 패션으로 표현해보세요',style:TextStyle(fontSize:15)),
+          TextButton(onPressed: onButtonClick,
+            child: Text('내 동네 설정하고 시작하기', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blue, fixedSize: Size(380, 45)
+              ),
+            )
+        ],
+      ),
     );
   }
 }
