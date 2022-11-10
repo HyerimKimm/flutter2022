131e3e69162d652301d976e51716f22b53e0a329
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2022-11-10 23:27:40 +0900

    stack 위젯을 사용해서 이미지 두개를 겹쳐줬고, stack 의 alignment 매개변수를 사용해서 위에 놓인 사진의 여백을 조정했음
    강의에선 LayoutBuilder로 화면의 가로, 세로 길이를 받아와서 Position에 뭔가 곱하기로 계산한 값을 대입하던데 굳이 그렇게 할 필요 없을 것 같았음..

--- lib/screens/start/intro_page.dart
+++ lib/screens/start/intro_page.dart
@@ -11,32 +11,43 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text('Hyerim\'s Market', style: TextStyle(color:Theme.of(context).primaryColor, fontSize:35, fontWeight: FontWeight.w800, fontFamily: 'bujangnimnunchi' )),
-          ExtendedImage.asset('assets/images/together.jpg',scale: 3,),
-          Text('패션 킬러 혜림 쇼핑몰',style:TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
-          Text('혜림 쇼핑몰은 항상 편하고 개성넘칩니다.\n혜림 쇼핑몰과 함께 당신을 패션으로 표현해보세요',style:TextStyle(fontSize:15)),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+    return LayoutBuilder(builder: (context, constraint){
+      Size size = MediaQuery.of(context).size;
+
+      final sizeOfPosXImg = size.width*0.1;
+      final sizeOfPosYImg = size.height*0.1;
+      return SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text('Hyerim\'s Market', style: TextStyle(color:Theme.of(context).primaryColor, fontSize:35, fontWeight: FontWeight.w800, fontFamily: 'bujangnimnunchi' )),
+            Stack(
+              alignment: Alignment(0,0.9),
               children: [
-                TextButton(
-                  onPressed: onButtonClick,
-                  child: Text('내 동네 설정하고 시작하기', style: TextStyle(color:Theme.of(context).primaryColorLight, fontSize: 18, fontWeight: FontWeight.bold, ),),
-                  style: TextButton.styleFrom(
-                      fixedSize: Size(380, 45), backgroundColor: Theme.of(context).primaryColor
+                ExtendedImage.asset('assets/images/together.jpg',scale: 2.5,),
+                ExtendedImage.asset('assets/images/loverens.png',scale: 6,),],
+            ),
+            Text('패션 킬러 혜림 쇼핑몰',style:TextStyle(fontSize:20, fontWeight: FontWeight.bold)),
+            Text('혜림 쇼핑몰은 항상 편하고 개성넘칩니다.\n혜림 쇼핑몰과 함께 당신을 패션으로 표현해보세요',style:TextStyle(fontSize:15)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextButton(
+                    onPressed: onButtonClick,
+                    child: Text('내 동네 설정하고 시작하기', style: TextStyle(color:Theme.of(context).primaryColorLight, fontSize: 18, fontWeight: FontWeight.bold, ),),
+                    style: TextButton.styleFrom(
+                        fixedSize: Size(380, 45), backgroundColor: Theme.of(context).primaryColor
                     ),
                   ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    });
   }
 }
