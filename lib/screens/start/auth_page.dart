8ffe535c71d98d0a8ec1b6d354fab835071a2022
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2022-11-23 00:24:18 +0900

    TextFormField의 keyboardType 매개변수를 이용해 숫자만 입력하도록 설정 전화번호 화면 구성 (내맘대로,, 강의 보면서 좋은 아이디어 배우기)

--- lib/screens/start/auth_page.dart
+++ lib/screens/start/auth_page.dart
@@ -1,12 +1,13 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
 import '../../constants/common_size.dart';
 
 class AuthPage extends StatelessWidget {
   AuthPage({Key? key}) : super(key: key);
 
   final inputBorder = OutlineInputBorder(borderSide: BorderSide(color: Colors.grey));
+  TextEditingController _controller = TextEditingController(text: '010');
 
   @override
   Widget build(BuildContext context) {
@@ -30,17 +31,45 @@ class AuthPage extends StatelessWidget {
                 ]),
                 SizedBox(height: common_padding,),
                 TextFormField(
+                  controller: _controller,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    MaskedInputFormatter("000 0000 0000")
+                  ],
                   decoration: InputDecoration(
                     focusedBorder: inputBorder, border: inputBorder,
                   ),
+                ),
+                SizedBox(height: 10,),
+                TextButton(onPressed: (){}, child: Text('인증번호 받기',style: TextStyle(color: Colors.white, fontSize: 20),),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColorDark,
+                      minimumSize: Size(400, 50)
+                  ),
+                ),
+                SizedBox(height: 10,),
+                TextFormField(
+                  controller: _controller,
                   keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    MaskedInputFormatter("000 0000 0000")
+                  ],
+                  decoration: InputDecoration(
+                    focusedBorder: inputBorder, border: inputBorder,
+                  ),
+                ),
+                SizedBox(height: 10,),
+                TextButton(onPressed: (){}, child: Text('인증번호 확인',style: TextStyle(color: Colors.white, fontSize: 20),),
+                  style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColorDark,
+                      minimumSize: Size(400, 50)
+                  ),
                 ),
               ],
             ),
           ),
         );
       },
-
     );
   }
 }
